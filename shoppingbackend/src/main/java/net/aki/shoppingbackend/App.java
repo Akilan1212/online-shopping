af5360e09bb1b55f8c5521405e8e2a90f945bf9a
akilan <karthiakilan15@gmail.com>
2018-02-22 10:00:43 +0530

    my project setup completed successfully

---
+++ shoppingbackend/src/main/java/net/aki/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.aki.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
